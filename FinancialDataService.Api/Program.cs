aea39c839eb84c86a691997055ffa45356fbf055
Savvas Koualis <s.koualis@hyperiosoftware.com>
2024-07-20 16:23:56 +0300

    Update service configuration and added additional packages

--- FinancialDataService.Api/Program.cs
+++ FinancialDataService.Api/Program.cs
@@ -1,14 +1,22 @@
+using FinancialDataService.Application.Extensions;
 using FinancialDataService.Infrastructure;
 using FinancialDataService.Infrastructure.Extensions;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Configure logging
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+builder.Logging.AddDebug();
+builder.Logging.AddEventSourceLogger();
+
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddInfrastructureServices(builder.Configuration);
+builder.Services.AddApplicationServices(builder.Configuration);
 
 var app = builder.Build();
 
