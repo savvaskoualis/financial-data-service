fde41a728962b24da56aa016b12ad0403c872081
Savvas Koualis <s.koualis@hyperiosoftware.com>
2024-07-22 00:36:07 +0300

    Added new Jobs project - Separating instrument/price fetching from Api

---
+++ FinancialDataService.Jobs/Program.cs
@@ -0,0 +1,36 @@
+﻿using FinancialDataService.Application.Interfaces;
+using FinancialDataService.Infrastructure.Extensions;
+using FinancialDataService.Infrastructure.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureAppConfiguration((hostingContext, configuration) =>
+    {
+        configuration.Sources.Clear();
+
+        var env = hostingContext.HostingEnvironment;
+
+        configuration
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+            .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
+
+        configuration.AddEnvironmentVariables();
+    })
+    .ConfigureLogging((hostingContext, logging) =>
+    {
+        logging.ClearProviders();
+        logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+        logging.AddConsole();
+    })
+    .ConfigureServices((context, services) =>
+    {
+        services.AddInfrastructureServices(context.Configuration);
+        services.AddStreams(context.Configuration);
+        services.AddJobs(context.Configuration);
+    })
+    .Build();
+
+await host.RunAsync();
